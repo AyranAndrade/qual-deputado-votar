8815168d5bd8d709f2baf3c4ae6c06c3135f3bc0
Ayran Andrade <ayranandrade@hotmail.com>
2020-10-04 16:51:18 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,3 @@
+url_base = 'https://especiais.g1.globo.com/politica/2019/o-voto-dos-deputados/?_ga=2.49234594.520651618.1601838599' \
+           '-1639340266.1601838598#/projetos/'
+
---
+++ projetos.py
@@ -0,0 +1,250 @@
+projetos = [
+  {
+    "value": "abertura-de-credito-extra-de-248-9-bi-reais",
+    "label": "Abertura de crédito extra de R$ 248,9 bi"
+  },
+  {
+    "value": "adiamento-das-eleicoes-de-2020-1-turno",
+    "label": "Adiamento das eleições de 2020 - 1º turno"
+  },
+  {
+    "value": "adiamento-das-eleicoes-de-2020-2-turno",
+    "label": "Adiamento das eleições de 2020 - 2º turno"
+  },
+  {
+    "value": "ajuda-a-estados-e-municipios-durante-a-pandemia",
+    "label": "Ajuda a estados e municípios durante a pandemia"
+  },
+  {
+    "value": "ajuda-a-estados-e-municipios-durante-a-pandemia-novo-texto",
+    "label": "Ajuda a estados e municípios durante a pandemia (novo texto)"
+  },
+  {
+    "value": "alteracao-no-codigo-de-transito",
+    "label": "Alteração no Código de Trânsito"
+  },
+  {
+    "value": "alteracao-no-fundo-eleitoral",
+    "label": "Alteração no Fundo Eleitoral"
+  },
+  {
+    "value": "ampliacao-do-auxilio-emergencial-a-agricultores-familiares",
+    "label": "Ampliação do auxílio emergencial a agricultores familiares"
+  },
+  {
+    "value": "ampliacao-do-capital-estrangeiro-a-empresas-aereas",
+    "label": "Ampliação do capital estrangeiro a empresas aéreas"
+  },
+  {
+    "value": "coaf-no-ministerio-da-justica",
+    "label": "Coaf no Ministério da Justiça"
+  },
+  {
+    "value": "cobranca-de-bagagem-por-companhias-aereas",
+    "label": "Cobrança de bagagem por companhias aéreas"
+  },
+  {
+    "value": "convocacao-do-ministro-abraham-weintraub",
+    "label": "Convocação do ministro Abraham Weintraub"
+  },
+  {
+    "value": "criacao-da-policia-penitenciaria-1-turno",
+    "label": "Criação da polícia penitenciária - 1º turno"
+  },
+  {
+    "value": "criacao-da-policia-penitenciaria-2-turno",
+    "label": "Criação da polícia penitenciária - 2º turno"
+  },
+  {
+    "value": "criacao-do-programa-medicos-pelo-brasil",
+    "label": "Criação do programa Médicos pelo Brasil"
+  },
+  {
+    "value": "criminalizacao-do-rompimento-de-barragem-por-negligencia",
+    "label": "Criminalização do rompimento de barragem por negligência"
+  },
+  {
+    "value": "destaque-para-afrouxar-congelamento-de-salarios",
+    "label": "Destaque para afrouxar congelamento de salários"
+  },
+  {
+    "value": "destaque-para-alterar-regra-para-homens-na-reforma-da-previdencia",
+    "label": "Destaque para alterar regra para homens na reforma da Previdência"
+  },
+  {
+    "value": "destaque-para-alterar-regra-para-mulheres-na-reforma-da-previdencia",
+    "label": "Destaque para alterar regra para mulheres na reforma da Previdência"
+  },
+  {
+    "value": "destaque-para-alterar-regra-para-professores-na-reforma-da-previdencia",
+    "label": "Destaque para alterar regra para professores na reforma da Previdência"
+  },
+  {
+    "value": "destaque-para-alterar-regras-para-policiais-na-reforma-da-previdencia",
+    "label": "Destaque para alterar regras para policiais na reforma da Previdência"
+  },
+  {
+    "value": "destaque-para-excluir-professores-da-reforma-da-previdencia",
+    "label": "Destaque para excluir professores da reforma da Previdência"
+  },
+  {
+    "value": "incentivo-a-construtoras-do-minha-casa-minha-vida",
+    "label": "Incentivo a construtoras do Minha Casa, Minha Vida"
+  },
+  {
+    "value": "mais-recursos-para-municipios-1-turno",
+    "label": "Mais recursos para municípios - 1º turno"
+  },
+  {
+    "value": "mp-da-estrutura-ministerial",
+    "label": "MP da estrutura ministerial"
+  },
+  {
+    "value": "mp-da-flexibilizacao-das-regras-trabalhistas-durante-a-pandemia",
+    "label": "MP da flexibilização das regras trabalhistas durante a pandemia"
+  },
+  {
+    "value": "mp-da-liberdade-economica",
+    "label": "MP da liberdade econômica"
+  },
+  {
+    "value": "mp-do-agro",
+    "label": "MP do Agro"
+  },
+  {
+    "value": "mp-do-aporte-bilionario-para-a-eletrobras",
+    "label": "MP do aporte bilionário para a Eletrobras"
+  },
+  {
+    "value": "mp-do-contrato-verde-e-amarelo",
+    "label": "MP do contrato verde e amarelo"
+  },
+  {
+    "value": "mudanca-na-renegociacao-de-dividas-com-a-uniao",
+    "label": "Mudança na renegociação de dívidas com a União"
+  },
+  {
+    "value": "nova-regra-para-repasse-de-emendas-1-turno",
+    "label": "Nova regra para repasse de emendas - 1º turno"
+  },
+  {
+    "value": "nova-regra-para-repasse-de-emendas-2-turno",
+    "label": "Nova regra para repasse de emendas - 2º turno"
+  },
+  {
+    "value": "novas-regras-para-o-iss",
+    "label": "Novas regras para o ISS"
+  },
+  {
+    "value": "novas-regras-para-partidos-e-eleicoes",
+    "label": "Novas regras para partidos e eleições"
+  },
+  {
+    "value": "novo-marco-legal-do-saneamento-basico",
+    "label": "Novo marco legal do saneamento básico"
+  },
+  {
+    "value": "pacote-anticrime",
+    "label": "Pacote anticrime"
+  },
+  {
+    "value": "pec-da-reforma-da-previdencia-1-turno",
+    "label": "PEC da reforma da Previdência - 1º turno"
+  },
+  {
+    "value": "pec-da-reforma-da-previdencia-2-turno",
+    "label": "PEC da reforma da Previdência - 2º turno"
+  },
+  {
+    "value": "pec-da-tramitacao-das-mps-1-turno",
+    "label": "PEC da tramitação das MPs - 1º turno"
+  },
+  {
+    "value": "pec-da-tramitacao-das-mps-2-turno",
+    "label": "PEC da tramitação das MPs - 2º turno"
+  },
+  {
+    "value": "pec-do-orcamento-de-guerra-1-turno",
+    "label": "PEC do orçamento de guerra - 1º turno"
+  },
+  {
+    "value": "pec-do-orcamento-de-guerra-2-turno",
+    "label": "PEC do orçamento de guerra - 2º turno"
+  },
+  {
+    "value": "pec-do-orcamento-de-guerra-novo-texto-1-turno",
+    "label": "PEC do orçamento de guerra (novo texto) - 1º turno"
+  },
+  {
+    "value": "pec-do-orcamento-de-guerra-novo-texto-2-turno",
+    "label": "PEC do orçamento de guerra (novo texto) - 2º turno"
+  },
+  {
+    "value": "pec-do-orcamento-impositivo-1-turno",
+    "label": "PEC do Orçamento Impositivo - 1º turno"
+  },
+  {
+    "value": "pec-do-orcamento-impositivo-2-turno",
+    "label": "PEC do Orçamento Impositivo - 2º turno"
+  },
+  {
+    "value": "pec-do-orcamento-impositivo-1-turno-novo-texto",
+    "label": "PEC do Orçamento Impositivo (novo texto) - 1º turno"
+  },
+  {
+    "value": "pec-do-orcamento-impositivo-2-turno-novo-texto",
+    "label": "PEC do Orçamento Impositivo (novo texto) - 2º turno"
+  },
+  {
+    "value": "posse-de-arma-em-toda-a-propriedade-rural",
+    "label": "Posse de arma em toda a propriedade rural"
+  },
+  {
+    "value": "prorrogacao-de-beneficios-fiscais-a-igrejas-e-instituicoes-beneficentes",
+    "label": "Prorrogação de benefícios fiscais a igrejas e instituições beneficentes"
+  },
+  {
+    "value": "reajuste-a-servidores-publicos",
+    "label": "Reajuste a servidores públicos"
+  },
+  {
+    "value": "reducao-do-fundo-eleitoral",
+    "label": "Redução do Fundo Eleitoral"
+  },
+  {
+    "value": "regras-para-armas-de-cacadores-e-colecionadores",
+    "label": "Regras para armas de caçadores e colecionadores"
+  },
+  {
+    "value": "remanejamento-de-verbas-para-a-saude",
+    "label": "Remanejamento de verbas para a saúde"
+  },
+  {
+    "value": "renovacao-do-fundeb-1-turno",
+    "label": "Renovação do Fundeb - 1º turno"
+  },
+  {
+    "value": "renovacao-do-fundeb-2-turno",
+    "label": "Renovação do Fundeb - 2º turno"
+  },
+  {
+    "value": "suspensao-do-mandato-do-deputado-wilson-santiago",
+    "label": "Suspensão do mandato do deputado Wilson Santiago"
+  },
+  {
+    "value": "uso-do-fundo-partidario-para-pagar-multas",
+    "label": "Uso do fundo partidário para pagar multas"
+  },
+  {
+    "value": "uso-obrigatorio-de-mascara-em-escola-e-comercio",
+    "label": "Uso obrigatório de máscara em escola e comércio"
+  },
+  {
+    "value": "veto-ao-limite-de-renda-para-acesso-ao-bpc",
+    "label": "Veto ao limite de renda para acesso ao BPC"
+  },
+  {
+    "value": "veto-sobre-a-propaganda-partidaria",
+    "label": "Veto sobre a propaganda partidária"
+  }
+]
