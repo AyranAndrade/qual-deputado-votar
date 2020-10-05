6a526130852dc38c34561c9493e13acad58eb67e
Ayran Andrade <ayranandrade@hotmail.com>
2020-10-05 01:09:57 -0300

    Adiciona script para inserir partidos no BD

---
+++ insere_partidos.py
@@ -0,0 +1,20 @@
+from partidos import partidos
+
+import pymysql
+
+db = pymysql.connect("localhost", "newuser", "", "qual_deputado_votar")
+
+cursor = db.cursor()
+
+for partido in partidos:
+    sql = "INSERT INTO partidos(nome_partido) VALUES ('%s')" % partido
+
+    try:
+        cursor.execute(sql)
+
+        db.commit()
+    except Exception as e:
+        print(e)
+        db.rollback()
+
+db.close()
